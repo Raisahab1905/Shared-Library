5c10e2c98762c85fe85d0b874db7244ab8cb13f8
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 18:34:14 +0530

    Update staticcodeanalysis.groovy

--- src/org/teamdowntime/template/react/staticcodeanalysis.groovy
+++ src/org/teamdowntime/template/react/staticcodeanalysis.groovy
@@ -2,17 +2,10 @@ package org.teamdowntime.template.react
 
 import org.teamdowntime.common.*
 
-class staticcodeanalysis implements Serializable {
-    def steps
-
-    staticcodeanalysis(steps) {
-        this.steps = steps
-    }
-
     def call(String branch, String url, String creds, String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
         def gitCheckOut = new checkout()
         def wsClean = new cleanworkspace()
-        def runSonarQubeAnalysis = new sonarqube(steps) // 👈 pass steps
+        def runSonarQubeAnalysis = new sonarqube() // 👈 pass steps
 
         wsClean.call()
         gitCheckOut.call(branch, url, creds)
