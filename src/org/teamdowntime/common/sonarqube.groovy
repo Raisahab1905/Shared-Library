d610186be95bb9a5108fab2a87815d3e78b066e8
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 18:12:50 +0530

    Update sonarqube.groovy

--- src/org/teamdowntime/common/sonarqube.groovy
+++ src/org/teamdowntime/common/sonarqube.groovy
@@ -1,15 +1,21 @@
 package org.teamdowntime.common
 
-def call(String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
-    // stage('Run SonarQube Analysis') {
-        withCredentials([string(credentialsId: sonarToken, variable: 'SONARQUBE_AUTH_TOKEN')]) {
-            sh """
-                /opt/sonar-scanner/bin/sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.sources=${sonarSources} \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${env.SONARQUBE_AUTH_TOKEN}
+class sonarqube implements Serializable {
+    def steps
+
+    sonarqube(steps) {
+        this.steps = steps
+    }
+
+    def call(String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
+        steps.withCredentials([steps.string(credentialsId: sonarToken, variable: 'SONARQUBE_AUTH_TOKEN')]) {
+            steps.sh """
+                /opt/sonar-scanner/bin/sonar-scanner \\
+                -Dsonar.projectKey=${projectKey} \\
+                -Dsonar.sources=${sonarSources} \\
+                -Dsonar.host.url=${sonarUrl} \\
+                -Dsonar.login=\$SONARQUBE_AUTH_TOKEN
             """
         }
-    // }
+    }
 }
