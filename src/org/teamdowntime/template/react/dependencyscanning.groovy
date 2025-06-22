433b6249052b2e444fb089ba1e592b401e5971ce
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 23:55:28 +0530

    Update dependencyscanning.groovy

--- src/org/teamdowntime/template/react/dependencyscanning.groovy
+++ src/org/teamdowntime/template/react/dependencyscanning.groovy
@@ -1,16 +1,19 @@
 package org.teamdowntime.template.react
 
 import org.teamdowntime.common.owasp
+import org.teamdowntime.common.notification  // ✅ class needs import
 
 def call(Map config) {
-    // Extract values from config
     def branch = config.get('branch', 'main')
     def url = config.get('repoUrl', '')
     def creds = config.get('credentialsId', '')
 
-    // These are global script steps (defined using def call()), so no need to instantiate
     cleanworkspace()
     checkout(branch, url, creds)
 
     owasp.run(this, config)
+
+    // ✅ Notify via Slack and Email
+    def notifier = new notification(this)
+    notifier.call(config)
 }
