ee8eba96bbdfb027d8d84b02ddefde0c2045cf11
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 13:40:43 +0530

    Update reactci.groovy

--- src/org/teamdowntime/template/reactci.groovy
+++ src/org/teamdowntime/template/reactci.groovy
@@ -1,39 +1,45 @@
-package org.teamdowntime.template
+package org.teamdowntimecrew.templates
 
 // Import all reusable classes
-import org.teamdowntimecrew.common.cleanWorkspace
-import org.teamdowntimecrew.common.checkout
-import org.teamdowntimecrew.common.dast
-import org.teamdowntimecrew.common.notification
-import org.teamdowntimecrew.common.dastrunner
+import org.teamdowntimecrew.common.CleanWorkspace
+import org.teamdowntimecrew.common.Checkout
+import org.teamdowntimecrew.common.DastRunner
+import org.teamdowntimecrew.common.Notification
 
 def call(Map pipelineConfig = [:]) {
-
     try {
-        stage('Clean Workspace') {
-            cleanWorkspace.run(this)
+        if (pipelineConfig.cleanworkspace) {
+            stage('Clean Workspace') {
+                CleanWorkspace.run(this)
+            }
         }
 
-        stage('Checkout Code') {
-            checkout.run(this, pipelineConfig.checkout)
+        if (pipelineConfig.checkout) {
+            stage('Checkout Code') {
+                Checkout.run(this, pipelineConfig.checkout)
+            }
         }
 
-        stage('DAST Scan') {
-        dastrunner.run(this, pipelineConfig.dast)
-       }
+        if (pipelineConfig.dast) {
+            stage('DAST Scan') {
+                DastRunner.run(this, pipelineConfig.dast)
+            }
+        }
 
-        // success notification
-        stage('Notify Success') {
-            pipelineConfig.notification.status = 'SUCCESS'
-            notification.send(this, pipelineConfig.notification)
+        if (pipelineConfig.notification) {
+            stage('Notify Success') {
+                pipelineConfig.notification.status = 'SUCCESS'
+                Notification.send(this, pipelineConfig.notification)
+            }
         }
 
     } catch (Exception e) {
-        // failure notification
-        stage('Notify Failure') {
-            pipelineConfig.notification.status = 'FAILURE'
-            pipelineConfig.notification.errorMessage = e.message
-            notification.send(this, pipelineConfig.notification)
+        if (pipelineConfig.notification) {
+            stage('Notify Failure') {
+                pipelineConfig.notification.status = 'FAILURE'
+                pipelineConfig.notification.errorMessage = e.message
+                Notification.send(this, pipelineConfig.notification)
+            }
         }
         currentBuild.result = 'FAILURE'
         throw e
