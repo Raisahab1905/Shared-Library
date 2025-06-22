58022bf13b1f9ef67433fe2e783a5ededf8eba8c
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 23:11:05 +0530

    Update dependencyscanning.groovy

--- src/org/teamdowntime/template/react/dependencyscanning.groovy
+++ src/org/teamdowntime/template/react/dependencyscanning.groovy
@@ -1,6 +1,6 @@
 package org.teamdowntime.template.react
 
-import org.teamdowntime.common.owasp
+import org.teamdowntime.common.owasp.groovy
 
 def call(Map config) {
     OwaspDepCheck.run(this, config)
