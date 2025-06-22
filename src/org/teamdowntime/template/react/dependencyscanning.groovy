73adb252eeb785c1c618a24a05b5eb77238f9600
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 23:35:25 +0530

    Update dependencyscanning.groovy

--- src/org/teamdowntime/template/react/dependencyscanning.groovy
+++ src/org/teamdowntime/template/react/dependencyscanning.groovy
@@ -3,14 +3,18 @@ package org.teamdowntime.template.react
 import org.teamdowntime.common.owasp
 import org.teamdowntime.common.cleanworkspace
 import org.teamdowntime.common.checkout
-import org.teamdowntime.common.notification
 
-def cleanworkspace = new cleanworkspace()
-def checkout = new checkout()
-
-cleanworkspace.call()
-checkout.call(branch, url, creds)
 def call(Map config) {
+    def cleanworkspace = new cleanworkspace()
+    def checkout = new checkout()
+
+    // Extract values from config
+    def branch = config.get('branch', 'main')
+    def url = config.get('repoUrl', '')
+    def creds = config.get('credentialsId', '')
+
+    cleanworkspace.call(this)
+    checkout.call(this, branch, url, creds)
+
     owasp.run(this, config)
 }
-
