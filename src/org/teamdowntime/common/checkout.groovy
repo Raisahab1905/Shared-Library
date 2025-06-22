b663ee295e979b1336f8f3e896d0c096eb84747c
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 19:03:13 +0530

    Update checkout.groovy

--- src/org/teamdowntime/common/checkout.groovy
+++ src/org/teamdowntime/common/checkout.groovy
@@ -3,6 +3,11 @@ package org.teamdowntime.common
 class checkout implements Serializable {
 
     def call(String branch, String repoUrl, String credsId) {
+        println "[INFO] Checkout Parameters:"
+        println "[INFO] Branch     : ${branch}"
+        println "[INFO] Repository : ${repoUrl}"
+        println "[INFO] Credentials: ${credsId}"
+
         checkout([
             $class: 'GitSCM',
             branches: [[name: "*/${branch}"]],
