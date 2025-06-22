e0663842d3d937ae890568ad1b224eed3f773275
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 23:46:11 +0530

    Update dependencyscanning.groovy

--- src/org/teamdowntime/template/react/dependencyscanning.groovy
+++ src/org/teamdowntime/template/react/dependencyscanning.groovy
@@ -3,18 +3,48 @@ package org.teamdowntime.template.react
 import org.teamdowntime.common.owasp
 import org.teamdowntime.common.cleanworkspace
 import org.teamdowntime.common.checkout
+import org.teamdowntime.common.notification
 
-def call(Map config) {
-    def cleanworkspace = new cleanworkspace()
-    def checkout = new checkout()
+class dependencyscanning implements Serializable {
+    def steps
 
-    // Extract values from config
-    def branch = config.get('branch', 'main')
-    def url = config.get('repoUrl', '')
-    def creds = config.get('credentialsId', '')
+    dependencyscanning(steps) {
+        this.steps = steps
+    }
 
-    cleanworkspace.call(this)
-    checkout.call(this, branch, url, creds)
+    def call(Map config) {
+        def cleanworkspace = new cleanworkspace()
+        def checkout = new checkout()
+        def notify = new notification(steps)
 
-    owasp.run(this, config)
+        def branch = config.get('branch', 'main')
+        def url = config.get('repoUrl', '')
+        def creds = config.get('credentialsId', '')
+
+        try {
+            cleanworkspace.call(steps)
+            checkout.call(branch, url, creds)
+            owasp.run(steps, config)
+
+            notify.call([
+                status       : 'SUCCESS',
+                emailTo      : config.get('emailTo'),
+                slackChannel : config.get('slackChannel'),
+                slackCredId  : config.get('slackCredId'),
+                buildTrigger : config.get('buildTrigger'),
+                reportLinks  : [[name: "OWASP Dependency Report", url: "${steps.env.BUILD_URL}dependency-check-report.html"]]
+            ])
+        } catch (err) {
+            notify.call([
+                status        : 'FAILURE',
+                emailTo       : config.get('emailTo'),
+                slackChannel  : config.get('slackChannel'),
+                slackCredId   : config.get('slackCredId'),
+                buildTrigger  : config.get('buildTrigger'),
+                failureReason : err.message,
+                failedStage   : steps.env.STAGE_NAME ?: 'unknown'
+            ])
+            throw err
+        }
+    }
 }
