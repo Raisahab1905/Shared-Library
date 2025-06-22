6155e24458fb968091a56343d4f85b2b02ba55ab
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 17:57:06 +0530

    Update staticcodeanalysis.groovy

--- src/org/teamdowntime/template/react/staticcodeanalysis.groovy
+++ src/org/teamdowntime/template/react/staticcodeanalysis.groovy
@@ -1,1 +1,13 @@
+package org.downtimecrew.template.react
 
+import org.teamdowntime.common.*
+
+def call(String branch, String url, String creds, String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
+    def gitCheckOut = new checkout()
+    def wsClean = new cleanworkspace()
+    def runSonarQubeAnalysis = new soanrqube()
+
+    wsClean.call()
+    gitCheckOut.call(branch, url, creds)
+    runSonarQubeAnalysis.call(projectKey, sonarUrl, sonarSources, sonarToken)
+}
