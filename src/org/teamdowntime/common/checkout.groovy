edc225107ecd38e13ea1df3d6a9f4e6bcd25b81b
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-23 00:18:39 +0530

    Update checkout.groovy

--- src/org/teamdowntime/common/checkout.groovy
+++ src/org/teamdowntime/common/checkout.groovy
@@ -1,9 +1,11 @@
 package org.teamdowntime.common
 
 def call(Map config = [:]) {
-    def branch     = config.get('branch', 'main')
-    def repoUrl    = config.get('repoUrl', '')
-    def credsId    = config.get('credentialsId', '')
+    def branch   = config.get('branch', 'main')
+    def repoUrl  = config.get('repoUrl', '')
+    def credsId  = config.get('credentialsId', '')
+
+    println "[INFO] Cloning from ${repoUrl} on branch ${branch} using credentials ${credsId}"
 
     checkout([
         $class: 'GitSCM',
