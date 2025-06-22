d0ce391b7a5d3ee89462539971ccf1e07d352428
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-23 00:15:16 +0530

    Update checkout.groovy

--- src/org/teamdowntime/common/checkout.groovy
+++ src/org/teamdowntime/common/checkout.groovy
@@ -1,14 +1,19 @@
 package org.teamdowntime.common
 
-    def call(String branch, String repoUrl, String credsId) {
+def call(Map config = [:]) {
+    def branch     = config.get('branch', 'main')
+    def repoUrl    = config.get('repoUrl', '')
+    def credsId    = config.get('credentialsId', '')
 
-        checkout([
-            $class: 'GitSCM',
-            branches: [[name: "*/${branch}"]],
-            userRemoteConfigs: [[
-                url: repoUrl,
-                credentialsId: credsId
-            ]]
-        ])
-    }
-
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[
+            url: repoUrl,
+            credentialsId: credsId
+        ]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        submoduleCfg: []
+    ])
+}
