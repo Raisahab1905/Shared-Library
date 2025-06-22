a760cc3de7370d0e5f49b1d0c9195f1d82d783b9
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 18:42:32 +0530

    Update sonarqube.groovy

--- src/org/teamdowntime/common/sonarqube.groovy
+++ src/org/teamdowntime/common/sonarqube.groovy
@@ -1,21 +1,20 @@
 package org.teamdowntime.common
 
 class sonarqube implements Serializable {
-    def steps
 
-    sonarqube(steps) {
-        this.steps = steps
-    }
+    def call(String projectKey, String sonarUrl, String sonarSources, String sonarTokenId) {
+        // sonarTokenId is the Jenkins credentialsId
+        def sonarToken = sh(
+            script: "echo \$(cat /tmp/sonar_token)", // this will be replaced by Jenkins credentials binding
+            returnStdout: true
+        ).trim()
 
-    def call(String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
-        steps.withCredentials([steps.string(credentialsId: sonarToken, variable: 'SONARQUBE_AUTH_TOKEN')]) {
-            steps.sh """
-                /opt/sonar-scanner/bin/sonar-scanner \\
-                -Dsonar.projectKey=${projectKey} \\
-                -Dsonar.sources=${sonarSources} \\
-                -Dsonar.host.url=${sonarUrl} \\
-                -Dsonar.login=\$SONARQUBE_AUTH_TOKEN
-            """
-        }
+        sh """
+            /opt/sonar-scanner/bin/sonar-scanner \\
+            -Dsonar.projectKey=${projectKey} \\
+            -Dsonar.sources=${sonarSources} \\
+            -Dsonar.host.url=${sonarUrl} \\
+            -Dsonar.login=${sonarToken}
+        """
     }
 }
