441a47a22a1602a8e5b89732d1cb35f27fb180ab
Prateek Rai <94138794+Raisahab1905@users.noreply.github.com>
2025-06-22 17:52:42 +0530

    Update sonarqube.groovy

--- src/org/teamdowntime/common/sonarqube.groovy
+++ src/org/teamdowntime/common/sonarqube.groovy
@@ -1,1 +1,15 @@
+package org.teamdowntime.common
 
+def call(String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
+    // stage('Run SonarQube Analysis') {
+        withCredentials([string(credentialsId: sonarToken, variable: 'SONARQUBE_AUTH_TOKEN')]) {
+            sh """
+                /opt/sonar-scanner/bin/sonar-scanner \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.sources=${sonarSources} \
+                -Dsonar.host.url=${sonarUrl} \
+                -Dsonar.login=${env.SONARQUBE_AUTH_TOKEN}
+            """
+        }
+    // }
+}
